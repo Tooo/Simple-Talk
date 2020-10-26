623277adc51e8417d45f2465b9ef5386c69565cd
tsc7 <tsc7@sfu.ca>
2020-10-25 23:05:20 -0700

    h file

---
+++ shutdownmanager.h
@@ -0,0 +1,13 @@
+#ifndef _SHUTDOWNMANAGER_H_
+#define _SHUTDOWNMANAGER_H_
+
+// Block current thread until some other thread calls triggerShutdown() method
+void ShutdownManager_waitForShutdown();
+
+// Indicate that we should shutdown, and allowing all threads which
+// blocked on waitForShutdown() function to continue
+void ShutdownManager_triggerShutdown();
+
+bool ShutdownManager_isShuttingDown();
+
+#endif
